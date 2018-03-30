769699041f8db723e9c980af6067de7ab187fd72
HuangHaoce <huang_haoce@163.com>
2018-03-30 11:30:21 +0800

    backend completed

--- resources/js/pat-list.js
+++ resources/js/pat-list.js
@@ -1,8 +1,10 @@
-var selectedPat = null
+hostname = '172.16.119.197:8080'
+selectedPat = null
+selectedBPRecord = null
+
 function addPat2List (tableType, patIde, patName, nextft) {
 
     let expGroup = $(`#${tableType}-group-list`)
-    console.log(expGroup)
     let newPatItem = $(`<tr id=\"pat-list-${patIde}\"></tr>`)
     let nameItem = $(`<td>${patName}</td>`)
     let nextftItem = $(`<td>${nextft}</td>`)
@@ -18,7 +20,7 @@ function addPat2List (tableType, patIde, patName, nextft) {
 
 function getAllPatients () {
     $.ajax({
-        url : "http://172.16.119.197:8080/copd/RCT/followup/getAllPatients",
+        url : `http://${hostname}/copd/RCT/followup/getAllPatients`,
         dataType : "json",
         success : function(data) {
             data.map( (item) => {
@@ -42,13 +44,16 @@ function getAllPatients () {
 function getSinglePatData ( PatIde ) {
     let patData = { "PatIde": PatIde }
     $.ajax({
-        url : "http://172.16.119.197:8080/copd/RCT/followup/getSinglePatData",
+        url : `http://${hostname}/copd/RCT/followup/getSinglePatData`,
         type: 'POST',        
         dataType : "json",
         data: patData,
         success : function(data) {
             console.log(data)
             setMainInfoPanel(data)
+            setBPRecordTable(data)            
+            setPatRiskFactor(data)
+            setFollowupTable(data)
         },
         error : function(data) {
             console.log(data)
@@ -67,8 +72,10 @@ function getMainInfoPanel () {
     return { PatName, PatIde, PatGroup, PatRiskLevel, PatAddress, PatPhone}
 }
 
-function setMainInfoPanel ( res ) {
-    var PatInfo = res.PatInfo
+function setMainInfoPanel ( data ) {
+
+    var PatInfo = data.PatInfo
+    console.log(PatInfo)
     var PatName = $('#main-pat-name')
     var PatIde = $('#main-pat-ide')
     var PatGroup = $('#main-pat-group')
@@ -82,14 +89,17 @@ function setMainInfoPanel ( res ) {
     PatRiskLevel.val(PatInfo.PatRiskLevel)
     PatAddress.val(PatInfo.PatAddress)
     PatPhone.val(PatInfo.PatPhone)
+
+    $('#next-followup-fromnow').html(PatInfo.FromNow + "天")
 }
 
 
 function updatePatInfo ( data ) {
+    if ( selectedPat === null ) return
     let patData = getMainInfoPanel()
     console.log(patData)
     $.ajax({
-        url : "http://172.16.119.197:8080/copd/RCT/followup/updatePatInfo",
+        url : `http://${hostname}/copd/RCT/followup/updatePatInfo`,
         type: 'POST',        
         dataType : "json",
         data: patData,
@@ -102,21 +112,95 @@ function updatePatInfo ( data ) {
     })
 }
 
+// 主面板中部的患者血压记录
+function addBPRecordItem (Id, Systolic, Diastolic, RecordTime) {
+    let bpRecordList = $(`#bp-record-list`)
+
+    let newBPItem = $(`<tr id=\"bp-list-${Id}\"></tr>`)
+    let sysItem = $(`<td>${Systolic}</td>`)
+    let diaItem = $(`<td>${Diastolic}</td>`)
+    let recordItem = $(`<td>${RecordTime}</td>`)
+
+    newBPItem.click(function(){
+        $(this).addClass("bp-item-active").siblings().removeClass("bp-item-active");
+        selectedBPRecord = $(this).attr("id").split('bp-record-')[1]
+    })
+    newBPItem.append(sysItem);
+    newBPItem.append(diaItem)
+    newBPItem.append(recordItem)
+    bpRecordList.append(newBPItem);
+}
+
+function clearBPRecordTable () {
+    let bpRecordList = $(`#bp-record-list`)
+    bpRecordList.empty()
+}
+
+function setBPRecordTable (data) {
+    const { PatBPMeasure } = data
+    clearBPRecordTable()
+    PatBPMeasure.map( item => {
+        addBPRecordItem( item.Id, item.Systolic, item.Diastolic, item.RecordTime)
+    })
+}
+
 // 主面板下的患者危险因素页面
-function getPatRiskFactor () {
+function getPatRiskFactor() {
+    var PatIde = selectedPat
     var TargetOrgan = $('#target-organ-select').val()
     var Compalication = $('#compalication-select').val()
-    var hypertension = $('#hypertension-select').val()
+    var HypertensionClassify = $('#hypertension-select').val()
     var RiskFactors = $('#risk-factors-select').val()
-    return { TargetOrgan, Compalication, hypertension, RiskFactors }
+    return { PatIde, TargetOrgan, Compalication, HypertensionClassify, RiskFactors }
 }
 
 function setPatRiskFactor (data) {
     const { PatRiskFactor } = data
-    $('#target-organ-select').val(PatRiskFactor.TargetCell || 0)    
+    $('#target-organ-select').val(PatRiskFactor.TargetOrgan || 0)    
     $('#compalication-select').val(PatRiskFactor.Compalication || 0)
     $('#hypertension-select').val(PatRiskFactor.HypertensionClassify || 0)
     $('#risk-factors-select').val(PatRiskFactor.RiskFactors || 0)
 }
 
-window.onload = getPatRiskFactor
+function updatePatRiskFactor () {
+    if ( selectedPat === null ) return
+    let patData = getPatRiskFactor()
+    console.log(patData)
+    $.ajax({
+        url : `http://${hostname}/copd/RCT/followup/updatePatRiskFactor`,
+        type: 'POST',        
+        dataType : "json",
+        data: patData,
+        success : function(data) {
+            console.log(data)
+        },
+        error : function(data) {
+            console.log(data)
+        }
+    })
+}
+
+// 渲染右侧的随访时间列表
+function addFollowupItem (FollowupTime, Done) {
+    let List = $(`#follow-up-list`)
+    let newItem = $(`<tr></tr>`)
+    let oneItem = $(`<td>${FollowupTime}</td>`)
+    var processing = 'x'
+    if (Done) processing = '√'
+    let twoItem = $(`<td>${processing}</td>`)
+
+    newItem.append(oneItem);
+    newItem.append(twoItem)
+    List.append(newItem);
+}
+function clearFollowupTable () {
+    $(`#follow-up-list`).empty()
+}
+function setFollowupTable (data) {
+    const {PatFollowup} = data
+    clearFollowupTable()
+    PatFollowup.map (item => {
+        addFollowupItem(item.FollowupTime, item.Done)
+    })
+}
+window.onload = getAllPatients
