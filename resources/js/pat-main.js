25602ac666334c67ca61a15014ac1851e81fa0fe
HuangHaoce <huang_haoce@163.com>
2018-03-30 14:40:30 +0800

    finish blood pressure record

--- resources/js/pat-main.js
+++ resources/js/pat-main.js
@@ -1,11 +1,19 @@
 hostname = '172.16.119.197:8080'
+
 selectedPat = null
-selectedBPRecord = null
+
+selectedBP = {
+    Id: null,
+    Systolic: null,
+    Diastolic: null,
+    RecordTime: null
+}
 
 function addPat2List (tableType, patIde, patName, nextft) {
 
     let expGroup = $(`#${tableType}-group-list`)
     let newPatItem = $(`<tr id=\"pat-list-${patIde}\"></tr>`)
+    let idItem = $(`<td>${patIde}</td>`)
     let nameItem = $(`<td>${patName}</td>`)
     let nextftItem = $(`<td>${nextft}</td>`)
     newPatItem.click(function(){
@@ -13,6 +21,7 @@ function addPat2List (tableType, patIde, patName, nextft) {
         selectedPat = $(this).attr("id").split('pat-list-')[1]
         getSinglePatData(selectedPat)
     })
+    newPatItem.append(idItem);
     newPatItem.append(nameItem);
     newPatItem.append(nextftItem)
     expGroup.append(newPatItem);
@@ -24,7 +33,7 @@ function getAllPatients () {
         dataType : "json",
         success : function(data) {
             data.map( (item) => {
-                let PatIde = item.PatIde || "未知id"
+                let PatIde = item.PatIde
                 let PatName = item.PatName || "未知姓名"
                 let PatGroup = item.PatGroup || "实验组"
                 let NextFollowupTime = item.NextFollowupTime || "没有排期"
@@ -49,7 +58,6 @@ function getSinglePatData ( PatIde ) {
         dataType : "json",
         data: patData,
         success : function(data) {
-            console.log(data)
             setMainInfoPanel(data)
             setBPRecordTable(data)            
             setPatRiskFactor(data)
@@ -75,7 +83,6 @@ function getMainInfoPanel () {
 function setMainInfoPanel ( data ) {
 
     var PatInfo = data.PatInfo
-    console.log(PatInfo)
     var PatName = $('#main-pat-name')
     var PatIde = $('#main-pat-ide')
     var PatGroup = $('#main-pat-group')
@@ -97,14 +104,12 @@ function setMainInfoPanel ( data ) {
 function updatePatInfo ( data ) {
     if ( selectedPat === null ) return
     let patData = getMainInfoPanel()
-    console.log(patData)
     $.ajax({
         url : `http://${hostname}/copd/RCT/followup/updatePatInfo`,
         type: 'POST',        
         dataType : "json",
         data: patData,
         success : function(data) {
-            console.log(data)
         },
         error : function(data) {
             console.log(data)
@@ -123,7 +128,11 @@ function addBPRecordItem (Id, Systolic, Diastolic, RecordTime) {
 
     newBPItem.click(function(){
         $(this).addClass("bp-item-active").siblings().removeClass("bp-item-active");
-        selectedBPRecord = $(this).attr("id").split('bp-record-')[1]
+        selectedBP.Id = $(this).attr("id").split('bp-list-')[1]
+        let BPTrList = $(this).children('td')
+        selectedBP.Systolic = BPTrList[0].innerHTML
+        selectedBP.Diastolic = BPTrList[1].innerHTML
+        selectedBP.RecordTime = BPTrList[2].innerHTML
     })
     newBPItem.append(sysItem);
     newBPItem.append(diaItem)
@@ -165,14 +174,12 @@ function setPatRiskFactor (data) {
 function updatePatRiskFactor () {
     if ( selectedPat === null ) return
     let patData = getPatRiskFactor()
-    console.log(patData)
     $.ajax({
         url : `http://${hostname}/copd/RCT/followup/updatePatRiskFactor`,
         type: 'POST',        
         dataType : "json",
         data: patData,
         success : function(data) {
-            console.log(data)
         },
         error : function(data) {
             console.log(data)
@@ -203,4 +210,170 @@ function setFollowupTable (data) {
         addFollowupItem(item.FollowupTime, item.Done)
     })
 }
-window.onload = getAllPatients
+
+/**
+ * 新增用户 modal 操作
+ */
+function addNewPatient () {
+    let patData = NewPatInfo()
+    $.ajax({
+        url : `http://${hostname}/copd/RCT/followup/addNewPatient`,
+        type: 'POST',        
+        dataType : "json",
+        data: patData,
+        success : function(data) {
+            if (data.result === 'ok'){
+                if (patData.PatGroup === '实验组') {
+                    addPat2List('exp', patData.patIde, patData.patName, null)
+                } else {
+                    addPat2List('ctr', patData.patIde, patData.patName, null)
+                }
+            } else {
+                alert(data.result || '添加出错')                
+            }
+        },
+        error : function(data) {
+            console.log(data)
+        }
+    })
+}
+function NewPatInfo() {
+    var PatIde = $('#newpat-id').val()
+    var PatName = $('#newpat-name').val()
+    var PatGroup = $('#newpat-group').val()
+    return { PatIde, PatName, PatGroup }
+}
+function clearNewPatModal () {
+    $('#newpat-id').val('')
+    $('#newpat-name').val('')
+    $('#newpat-group').val('实验组')
+}
+function showAddNewPatModal () {
+    clearNewPatModal()
+    $('#addNewPatModal').modal('show')
+}
+function commitAddNewPatModal () {
+    addNewPatient()
+    $('#addNewPatModal').modal('hide')
+}
+
+/**
+ * 增改删 血压记录操作
+ */
+function setBPReocrdPatModal (Systolic, Diastolic, recordTime) {
+    $('#sys-record-input').val(Systolic)
+    $('#dia-record-input').val(Diastolic)
+    $('#bp-recordtime').val(recordTime)
+}
+function getBPReocrdPatModal () {
+    var Systolic = $('#sys-record-input').val()
+    var Diastolic = $('#dia-record-input').val()
+    var RecordTime = $('#bp-recordtime').val()
+    return { Systolic, Diastolic, RecordTime }
+}
+function showAddBPRecordModal () {
+    if (selectedPat === null) return 
+    setBPReocrdPatModal('', '', '')
+    $('#commit-btn').attr("onclick","addBPRecord();");
+    $('#updateBPRecordModal').modal('show')
+    
+}
+function showAlterBPRecordModal () {
+    if (selectedBP.Id === null) return    
+    $('#commit-btn').attr("onclick","updateBPRecord();");
+    setBPReocrdPatModal(selectedBP.Systolic,selectedBP.Diastolic,selectedBP.RecordTime)
+    $('#updateBPRecordModal').modal('show')
+}
+
+function addBPRecord ( ) {
+    bpData = getBPReocrdPatModal()
+    bpData.PatIde = selectedPat 
+    $.ajax({
+        url : `http://${hostname}/copd/RCT/followup/insertBPRecord`,
+        type: 'POST',        
+        dataType : "json",
+        data: bpData,
+        success : function(data) {
+            if (data.result !== 'failed'){
+                var newItemId = data.result
+                addBPRecordItem(newItemId, bpData.Systolic, bpData.Diastolic, bpData.RecordTime)
+            } else {
+                alert(data.result)
+            }  
+        },
+        error : function(data) {
+            console.log(data)
+        }
+    })
+    $('#updateBPRecordModal').modal('hide')    
+}
+function updateBPRecord ( ) {
+    bpData = getBPReocrdPatModal()
+    bpData.Id = selectedBP.Id
+    $.ajax({
+        url : `http://${hostname}/copd/RCT/followup/updateBPRecord`,
+        type: 'POST',        
+        dataType : "json",
+        data: bpData,
+        success : function(data) {
+            if (data.result === 'ok'){
+                var bp = $(`#bp-list-${bpData.Id}`).children('td')
+                bp[0].innerHTML = bpData.Systolic
+                bp[1].innerHTML = bpData.Diastolic
+                bp[2].innerHTML = bpData.RecordTime
+                selectedBP.Systolic = bpData.Systolic
+                selectedBP.Diastolic = bpData.Diastolic
+                selectedBP.RecordTime = bpData.RecordTime
+            } else {
+                alert(data.result)
+            }
+        },
+        error : function(data) {
+            console.log(data)
+        }
+    })
+    $('#updateBPRecordModal').modal('hide')    
+}
+
+function deleteBPRecord () {
+    if (selectedBP.Id === null) return  
+    bpData = {'Id': selectedBP.Id}  
+    $.ajax({
+        url : `http://${hostname}/copd/RCT/followup/deleteBPRecord`,
+        type: 'POST',        
+        dataType : "json",
+        data: bpData,
+        success : function(data) {
+            if (data.result === 'ok'){
+                addBPRecordItem
+                $(`#bp-list-${selectedBP.Id}`).remove()
+            } else {
+                alert(data.result)
+            }
+        },
+        error : function(data) {
+            console.log(data)
+        }
+    })
+}
+
+// 运行函数
+function onStart () {
+    getAllPatients()
+    $('#bp-recordtime').datetimepicker({
+        language:  'ch',  
+        weekStart: 1,  
+        todayBtn:  0,  
+        autoclose: 1,  
+        todayHighlight: 1,  
+        startView: 1,  
+        minView: 0,  
+        maxView: 1,  
+        forceParse: 0,  
+        //分钟间隔  
+        minuteStep: 1  
+    })
+}
+
+
+window.onload = onStart
